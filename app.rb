59c0b70b3e20cd28f8853768da513aba0f0583bc
Aaron Unger <aaron.unger@gmail.com>
2014-01-02 12:46:34 -0800

    Add jcl template and routes

--- app.rb
+++ app.rb
@@ -6,9 +6,11 @@ require File.dirname(__FILE__) +'/lib/wrapper'
 # set :static, true
 set :public, File.dirname(__FILE__) + '/public'
 set :install_template, File.dirname(__FILE__) + '/templates/install.jnlp'
+set :jcl_template, File.dirname(__FILE__) + '/templates/jcl.jnlp'
 mime :jnlp, 'application/x-java-jnlp-file'
 
 Wrapper.add_template(:install, open(install_template).read)
+Wrapper.add_template(:jcl, open(jcl_template).read)
 
 def raw_post
   request.env["rack.input"].read
@@ -31,6 +33,19 @@ get '/:project/:version.jnlp' do
     :old_versions => params[:old_versions],
     :default_jnlp => params[:default_jnlp],
     :max_heap => params[:max_heap]
+  })
+end
+
+get '/jcl/:project/:version.jnlp' do
+  content_type :jnlp
+  last_modified(Time.now)
+  Wrapper.wrap({
+    :template => :jcl,
+    :wrapped_jnlp => params[:jnlp],
+    :href => request.url,
+    :vendor => "ConcordConsortium",
+    :project => "General",
+    :version => "1.0"
   })
 end
 
