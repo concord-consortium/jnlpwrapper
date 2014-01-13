f4a83165148f42b1638f8c8c23c644705f16520f
Aaron Unger <aaron.unger@gmail.com>
2014-01-13 14:07:15 -0800

    If the client is on OS X 10.9, deliver a custom launcher file type

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ require 'wrapper'
 configure do
   set :public_folder, File.dirname(__FILE__) + '/public'
   mime_type :jnlp, 'application/x-java-jnlp-file'
+  mime_type :ccla, 'application/vnd.concordconsortium.launcher'
 
   Wrapper.add_template(:install, open(File.dirname(__FILE__) + '/templates/install.jnlp').read)
   Wrapper.add_template(:jcl, open(File.dirname(__FILE__) + '/templates/jcl.jnlp').read)
@@ -19,12 +20,38 @@ def raw_post
   request.env["rack.input"].read
 end
 
+def useragent
+  UserAgent.parse(request.user_agent)
+end
+
 get '/' do
   redirect '/index.html'
 end
 
 get '/:project/:version.jnlp' do
   content_type :jnlp
+  last_modified(Time.now)
+  ua = useragent
+  if ua.platform == "Macintosh" && ua.os =~ /10\.9(\.\d+)?$/
+    #redirect to the ccla file
+    redirect request.fullpath.sub(/\.jnlp/, '.ccla')
+  else
+    Wrapper.wrap({
+      :template => :install,
+      :wrapped_jnlp => params[:jnlp],
+      :href => request.url,
+      :vendor => "ConcordConsortium",
+      :project => params[:project],
+      :version => params[:version],
+      :old_versions => params[:old_versions],
+      :default_jnlp => params[:default_jnlp],
+      :max_heap => params[:max_heap]
+    })
+  end
+end
+
+get '/:project/:version.ccla' do
+  content_type :ccla
   last_modified(Time.now)
   Wrapper.wrap({
     :template => :install,
@@ -41,6 +68,25 @@ end
 
 get '/jcl/:project/:version.jnlp' do
   content_type :jnlp
+  last_modified(Time.now)
+  ua = useragent
+  if ua.platform == "Macintosh" && ua.os =~ /10\.9(\.\d+)?$/
+    #redirect to the ccla file
+    redirect request.fullpath.sub(/\.jnlp/, '.ccla')
+  else
+    Wrapper.wrap({
+      :template => :jcl,
+      :wrapped_jnlp => params[:jnlp],
+      :href => request.url,
+      :vendor => "ConcordConsortium",
+      :project => "General",
+      :version => "1.0"
+    })
+  end
+end
+
+get '/jcl/:project/:version.ccla' do
+  content_type :ccla
   last_modified(Time.now)
   Wrapper.wrap({
     :template => :jcl,
